fbc19d86a9567aa65667e8eb9184fbdb10251427
Taiki <taiki.iwahori.bn@furuno.co.jp>
2022-11-09 03:37:25 +0900

    ENH: ファイル名をファイルの元ファイルの最終更新日時にした。 アップロードが簡単になるようにするため。

--- merge_movie.py
+++ merge_movie.py
@@ -1,13 +1,20 @@
+import datetime
 from pathlib import Path
 
 from moviepy import editor
 
 
 def main():
-    list_path_mp4 = [path for path in Path("data").glob("*.mp4")]
+    path_data_dir = Path("G:/DCIM/100GOPRO")
+    list_path_mp4 = [path for path in path_data_dir.glob("*.mp4")]
     list_path_mp4.sort()
 
-    path_output_mp4 = Path("hoge.mp4")
+    latest_file_stat = list_path_mp4[0].stat()
+    datetime_latest_file_mtime = datetime.datetime.fromtimestamp(
+        latest_file_stat.st_mtime
+    )  # mtimeはファイルの最終更新日時.JSTかUTC化に注意。windowsでやった時はなぜかJSTになってた
+
+    path_output_mp4 = Path(f"{datetime_latest_file_mtime.strftime('%Y%m%d')}.mp4")
 
     # 動画情報の取得
     list_clip = [editor.VideoFileClip(str(path_mp4)) for path_mp4 in list_path_mp4]
