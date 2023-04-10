4dd2b2fba1b34b498eeff639547404f19289bf67
Taiki <taiki.iwahori.bn@furuno.co.jp>
2023-04-10 22:30:48 +0900

    CLN: 引数の受け取りはいったん無視して認証まで動くようにした

--- upload_video.py
+++ upload_video.py
@@ -9,7 +9,6 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
 from google_auth_oauthlib.flow import InstalledAppFlow
-from google_auth_oauthlib.tools import argparser
 from google.auth.transport.requests import Request
 
 """
@@ -113,7 +112,33 @@ def get_authenticated_service():
     )
 
 
-def initialize_upload(youtube, options):
+def initialize_upload(youtube):
+    # argparser.add_argument("--file", required=True, help="Video file to upload")
+    # argparser.add_argument("--title", help="Video title", default="Test Title")
+    # argparser.add_argument(
+    #     "--description", help="Video description", default="Test Description"
+    # )
+    # argparser.add_argument(
+    #     "--category",
+    #     default="22",
+    #     help="Numeric video category. "
+    #     + "See https://developers.google.com/youtube/v3/docs/videoCategories/list",
+    # )
+    # argparser.add_argument(
+    #     "--keywords", help="Video keywords, comma separated", default=""
+    # )
+    # argparser.add_argument(
+    #     "--privacyStatus",
+    #     choices=VALID_PRIVACY_STATUSES,
+    #     default=VALID_PRIVACY_STATUSES[0],
+    #     help="Video privacy status.",
+    # )
+    # args = argparser.parse_args()
+
+    # if not os.path.exists(args.file):
+    #     exit("Please specify a valid file using the --file= parameter.")
+
+    options=None
     tags = None
     if options.keywords:
         tags = options.keywords.split(",")
@@ -188,33 +213,8 @@ def resumable_upload(insert_request):
 
 
 if __name__ == "__main__":
-    argparser.add_argument("--file", required=True, help="Video file to upload")
-    argparser.add_argument("--title", help="Video title", default="Test Title")
-    argparser.add_argument(
-        "--description", help="Video description", default="Test Description"
-    )
-    argparser.add_argument(
-        "--category",
-        default="22",
-        help="Numeric video category. "
-        + "See https://developers.google.com/youtube/v3/docs/videoCategories/list",
-    )
-    argparser.add_argument(
-        "--keywords", help="Video keywords, comma separated", default=""
-    )
-    argparser.add_argument(
-        "--privacyStatus",
-        choices=VALID_PRIVACY_STATUSES,
-        default=VALID_PRIVACY_STATUSES[0],
-        help="Video privacy status.",
-    )
-    args = argparser.parse_args()
-
-    if not os.path.exists(args.file):
-        exit("Please specify a valid file using the --file= parameter.")
-
     youtube = get_authenticated_service()
     try:
-        initialize_upload(youtube, args)
+        initialize_upload(youtube)
     except HttpError as e:
         print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
